70f7842ad87efb44e43a9c701eef5a05727b9215
Nakul Nayak <79224511+nnayk@users.noreply.github.com>
2022-10-26 15:15:24 -0700

    Update Utility.js

--- Utility.js
+++ Utility.js
@@ -1,16 +1,4 @@
 function getPercentChange(old_price, new_price) {
     return ((new_price-old_price)/old_price)*100;
   }
-  
-  // function processedTickers(tickers)
-  // {
-  //   let updatedTickers = []
-  
-  //   for (const tick of tickers)
-  //   {
-  //     updatedTickers.push(tick.replaceAll('.','-'))
-  //   }
-  
-  //   return updatedTickers;
-  // }
-  
+ 
