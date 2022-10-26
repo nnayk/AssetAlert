2018c1ea90eaa6d3127f6145bb0819a74ba9fc6b
Nakul Nayak <79224511+nnayk@users.noreply.github.com>
2022-10-26 15:13:25 -0700

    Update DateInfo.js

--- DateInfo.js
+++ DateInfo.js
@@ -1,14 +1,7 @@
 function getMonth()
 {
-  // let URL = "https://paper-api.alpaca.markets";
 let today = new Date();
 return String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-
-// today = mm + '/' + dd + '/' + yyyy;
-
-// console.log(`DATE ${today}`);
-// formula=`=INDEX(GOOGLEFINANCE("EPD","PRICE",DATE(${yyyy},${mm},${dd})),2,2)`;
-// spreadsheet.getRange('K8').setValue(formula);
 }
 
 function getPrevMonth()
